1a2991dc1d196fafb0abe629ebfc0de7ed21324f
nathanjh-28 <nathan@nathanjharris.com>
2020-10-16 16:27:41 -0600

    fixed some problems with edit profile and added custom error templates

--- app/routes.py
+++ app/routes.py
@@ -82,7 +82,7 @@ def edit_profile():
     form = EditProfileForm()
     if form.validate_on_submit():
         current_user.username = form.username.data
-        current_user.username = form.about_me.data
+        current_user.about_me = form.about_me.data
         db.session.commit()
         flash('Your changes have been saved.')
         return redirect(url_for('edit_profile'))
@@ -90,4 +90,3 @@ def edit_profile():
         form.username.data = current_user.username
         form.about_me.data = current_user.about_me
     return render_template('edit_profile.html', title='Edit Profile', form=form)
-    
